03cae95bf5e740131206d6d80a582d136b770290
Александра Федяева <aleksandra.fedyaeva@gmail.com>
2025-04-19 22:32:51 +0400

    Задание 5. Структура программы, переменные. Площадь круга

---
+++ Task5/Program.cs
@@ -0,0 +1,4 @@
+﻿const double pi = Math.PI;
+double radius = 10;
+double areaOfCircle = pi * radius * radius;
+Console.Write("Площадь круга радиусом " + radius + " равна " + Math.Round(areaOfCircle, 2) + ".");
