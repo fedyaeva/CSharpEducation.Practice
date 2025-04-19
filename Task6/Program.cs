05869f0d9f646c0c62fd22af5595c46fd478c850
Александра Федяева <aleksandra.fedyaeva@gmail.com>
2025-04-19 22:35:32 +0400

    Задание 6. Структура программы, переменные. Определение гипотенузы и катета

---
+++ Task6/Program.cs
@@ -0,0 +1,7 @@
+﻿double catetA = 3;
+double catetB = 4;
+double hypotinuse = 5;
+double unknownHypotinuse = Math.Sqrt(catetA*catetA + catetB*catetB);
+Console.Write("В треугольнике с катетом " + catetA + " и катетом " + catetB + " гипотенуза равна " + unknownHypotinuse + "\n");
+double unknownCatet = Math.Sqrt(hypotinuse*hypotinuse - catetA*catetA);
+Console.Write("В треугольнике с катетом " + catetA + " и гипотенузой " + hypotinuse + " второй катет равен " + unknownCatet);
