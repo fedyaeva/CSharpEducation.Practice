827603dafef6e4938eeba25e92cf9944bfafb630
Александра Федяева <aleksandra.fedyaeva@gmail.com>
2025-04-19 22:28:58 +0400

    Задание 4. Структура программы, переменные. Вывод объединенной фразы в консоль

---
+++ Task4/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Write("Введите первую фразу: ");
+string phrase1 = Console.ReadLine();
+Console.Write("Введите вторую фразу: ");
+string phrase2 = Console.ReadLine();
+Console.WriteLine(phrase1 + " " + phrase2);
