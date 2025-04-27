3cd36f5e2962d4e9649d932744f4dd8147f7efe3
Александра Федяева <aleksandra.fedyaeva@gmail.com>
2025-04-27 11:15:47 +0400

    Практика 2. Переименование проектов из практики 1.

---
+++ Practice2.Task1/Program.cs
@@ -0,0 +1,71 @@
+﻿namespace Practice2.Task1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+        //a. цикл for. Пусть он выводит в консоль значение своего же счётчика
+        Console.WriteLine("Задание 1a");
+        for (int i = 0; i < 10; i++)
+        {
+            Console.WriteLine("for  " + i);
+        }
+
+        //b. цикл while. Пусть он выводит в консоль значение своего же счётчика
+        Console.WriteLine("Задание 1b");
+        int b = 0;
+        while(b < 10)
+        {
+            Console.WriteLine("while  " + b);
+            b++;
+        }
+        
+        
+        //c. цикл do while. Пусть он выводит в консоль значение своего же счётчика.
+        Console.WriteLine("Задание 1c");
+        int c = 0;
+        do
+        {
+        Console.WriteLine("do while " + c);
+        c++;
+        } while (c < 10);
+
+        //d. цикл for, в котором у пользователя запрашиваются слова, затем выводится полная фраза, слова в которой разделены пробелами
+        Console.WriteLine("Задание 1d");
+        string[] text1 = new string[4];
+        for (int d = 0; d < 4; d++)
+        {
+            Console.WriteLine($"Введите слово № {d + 1}: ");
+            text1[d] = Console.ReadLine();
+        }
+        String sentence1 = string.Join(" ", text1);;
+        Console.WriteLine($"Фраза для цикла for: {sentence1}");
+        
+        //e. цикл while, в котором у пользователя запрашиваются слова, затем выводится полная фраза, слова в которой разделены пробелами.
+        Console.WriteLine("Задание 1e");
+        int e = 0;
+        string[] text2 = new string[4];
+        while (e < 4)
+        {
+            Console.WriteLine($"Введите слово № {e + 1}: ");
+            text2[e] = Console.ReadLine();
+            e++;
+        }
+        String sentence2 = string.Join(" ", text2);;
+        Console.WriteLine($"Фраза для цикла while: {sentence2}");
+        
+        //f. цикл do while, в котором у пользователя запрашиваются слова, затем выводится полная фраза, слова в которой разделены пробелами
+        Console.WriteLine("Задание 1f");
+        int f = 0;
+        string[] text3 = new string[4];
+        do
+        {
+            Console.WriteLine($"Введите слово № {f + 1}: ");
+            text3[f] = Console.ReadLine();
+            f++;
+        } while (f < 4);
+        String sentence3 = string.Join(" ", text3);;
+        Console.WriteLine($"Фраза для цикла do while: {sentence3}");
+    }
+}
---
+++ Practice2.Task2/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Practice2.Task2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //a. метры в километры
+        Console.WriteLine("Задание 2a");
+        double meter = 5000;
+        double kilometer = meter / 1000;
+        Console.WriteLine($"{meter} метров = {kilometer} километров");
+        
+        //b. километры в сантиметры
+        Console.WriteLine("Задание 2b");
+        double centimeter = kilometer * 100000;
+        Console.WriteLine($"{kilometer} километров = {centimeter} сантиметров");
+        
+        //c. м/с в км/ч
+        Console.WriteLine("Задание 2c");
+        double speedMeterPerSec = 1234;
+        double speedKilometerPerHour = speedMeterPerSec * 3.6;
+        Console.WriteLine($"{speedMeterPerSec} м/с = {Math.Round(speedKilometerPerHour, 2)} км/ч");
+        
+        //d. градусы C в градусы F
+        Console.WriteLine("Задание 2d");
+        double temperatureС = -23;
+        double temperatureF = temperatureС * 1.8 + 32;
+        Console.WriteLine($"{temperatureС} С = {Math.Round(temperatureF, 2)} F");
+    }
+}
