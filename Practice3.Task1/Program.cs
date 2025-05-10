cf348bf90e1dc99561de1f63e6d356fd237977db
fedyaeva <aleksandra.fedyaeva@gmail.com>
2025-05-10 21:31:59 +0400

    Практика 3. Задание 1

---
+++ Practice3.Task1/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace Practice3.Task1;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Student student = new Student();
+        student.InfoAboutStudent();
+    }
+}
---
+++ Practice3.Task1/Student.cs
@@ -0,0 +1,10 @@
+namespace Practice3.Task1;
+
+public class Student
+{
+    string name = "Саша";
+    int age = 32;
+    public void InfoAboutStudent() {
+       Console.WriteLine($"Имя студента: {name}, возраст студента: {age}"); 
+    }
+}
